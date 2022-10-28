db7ef6d76df83aa1577dc308e7e2f191e1d26dc6
Manuel Rangel <salvatorex89@gmail.com>
2022-10-28 15:32:02 -0500

    Add Axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import {
+  Button,
+  Grid,
+  Box
+} from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import axios from 'axios';
 import './App.css';
 
+const cols = [
+  { field: 'id', headerName: 'ID', width: 90 },
+  { field: 'name', headerName: 'Nombre', width: 90 },
+  { field: 'ced', headerName: 'Cédula', width: 90 },
+];
+
+
+
 function App() {
+
+  const [rowsShow, setRowShow] = useState([]);
+  axios.get('')
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={1} padding={3}>
+          <Grid container spacing={1} >
+            {/* <Grid item md={6} sm={6} sx={{ background: '#CCC' }}>
+              <Button
+                variant='filled'
+              >
+                Click
+              </Button>
+            </Grid>
+            <Grid item md={6} sm={6} sx={{ height: '40vh' }} >
+              <DataGrid
+                columns={cols}
+                rows={rowsShow}
+              />
+            </Grid> */}
+          </Grid>
+        </Grid>
+      </Box>
     </div>
   );
 }
